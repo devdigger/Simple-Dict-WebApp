659d427ba30e721870b1a663217a2a93703f6d33
devdigger <aec.cse.sagarkumar@gmail.com>
2023-06-26 22:04:48 +0530

    handling error

--- dictionary/views.py
+++ dictionary/views.py
@@ -7,32 +7,38 @@ from freedictionaryapi.clients.sync_client import DictionaryApiClient
 def perform_search(query):
     # Perform your dictionary search logic here and return the results
     # This is just a sample implementation
+    data={}
     print('In perform search')
-    with DictionaryApiClient() as client:
-        parser = client.fetch_parser(query)
-    word=parser.word
-    data={
-    }
-    print("Word = ",word.word)
-    print("Phonetics = ",word.phonetics[0].text)
-    data['audio'] = word.phonetics[1].audio
-    data['word'] = word.word
-    data['phonetics'] = word.phonetics[0].text
-    for rows in word.meanings:
-        print(rows.part_of_speech.upper() ,":")
-
-        example=str(rows.definitions[0].example) if rows.definitions[0].example is not None  else "N/A"
-        synonyms = ",".join(list(map(str, rows.definitions[0].synonyms))) if len(rows.definitions[0].synonyms)>0 else "N/A"
-        data[rows.part_of_speech.upper()] = {
-            "definition" :  str(rows.definitions[0].definition),
-            "example" : example,
-            "synonyms" : synonyms      
+    try:
+        with DictionaryApiClient() as client:
+            parser = client.fetch_parser(query)
+    
+        word=parser.word
+        data={
         }
-        #print(dir(rows.definitions[0]))
-        print("Defintion : ",rows.definitions[0].definition)
-        print("Example : ",example)
-        print("Synonyms : ",synonyms)
+        print("Word = ",word.word)
+        print(word.phonetics)
+        print("Phonetics = ",word.phonetics[0].text)
+        data['audio'] = word.phonetics[0].audio
+        data['word'] = word.word
+        data['phonetics'] = word.phonetics[0].text if word.phonetics[0].text is not None else "Phonetics N/A"
 
+        for rows in word.meanings:
+            print(rows.part_of_speech.upper() ,":")
+
+            example=str(rows.definitions[0].example) if rows.definitions[0].example is not None  else "N/A"
+            synonyms = ",".join(list(map(str, rows.definitions[0].synonyms))) if len(rows.definitions[0].synonyms)>0 else "N/A"
+            data[rows.part_of_speech.upper()] = {
+                "definition" :  str(rows.definitions[0].definition),
+                "example" : example,
+                "synonyms" : synonyms      
+            }
+            #print(dir(rows.definitions[0]))
+            print("Defintion : ",rows.definitions[0].definition)
+            print("Example : ",example)
+            print("Synonyms : ",synonyms)
+    except Exception as e:
+        print(str(e))
     print(data)
     return data
 
